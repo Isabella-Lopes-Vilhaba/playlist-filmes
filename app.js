ab986a2a4bd2c74413908ac781bf99b7d4c27f41
Isabella Lopes Vilhaba <isabellavilhaba@gmail.com>
2021-04-22 00:06:45 -0300

    Estiliza footer

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 function comedia(){
-  
+
   var imgComedia = ["https://m.media-amazon.com/images/M/MV5BMTk3NTM4NDg3OF5BMl5BanBnXkFtZTcwNDk3MzEwNQ@@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BYzRmOTYyZDgtZWJmMy00OTI1LWExMjgtZjRmYTJiNzgwMGIwXkEyXkFqcGdeQXVyNjEwNTM2Mzc@._V1_UY268_CR16,0,182,268_AL_.jpg"];
   var linkComedia = ["https://www.imdb.com/title/tt1488555/?ref_=nv_sr_srsg_0", "https://www.imdb.com/title/tt14111726/?ref_=fn_al_tt_1"];
   var nomeComedia = ["Eu Queria Ter a Sua Vida", "Cabras da Peste"];
-  
   
   var resultado = document.getElementById("resultado");
   resultado.innerHTML = ("");
@@ -11,13 +10,12 @@ function comedia(){
   for (var i = 0; i < imgComedia.length; i++) {
     var categoria = document.getElementById("categoria");
     categoria.innerHTML = ("<h2>Filmes de Comédia</h2>");
-    resultado.innerHTML += ('<div class="conteudo"><div class="imagem"><a href=' + linkComedia[i] + ' target="_blank"><img src=' + imgComedia[i] + ' class="cartaz"></a></div> <h3>' + nomeComedia[i] + '</h3></div>');
-    
+    resultado.innerHTML += ('<section class="conteudo"><section class="imagem"><a href=' + linkComedia[i] + ' target="_blank"><img src=' + imgComedia[i] + ' class="cartaz"></a></section> <h3>' + nomeComedia[i] + '</h3></section>');
   }
   }
 
 function romance(){
-  
+
   var imgRomance = ["https://m.media-amazon.com/images/M/MV5BMjQ3NjM5MTAzN15BMl5BanBnXkFtZTgwODQzMDAwNjM@._V1_UX182_CR0,0,182,268_AL_.jpg"];
   var linkRomance = ["https://www.imdb.com/title/tt3846674/?ref_=nv_sr_srsg_0"];
   var nomeRomance = ["Para Todos os Garotos que já Amei"];
@@ -28,12 +26,12 @@ function romance(){
   for (var i = 0; i < imgRomance.length; i++) {
     var categoria = document.getElementById("categoria");
     categoria.innerHTML = ("<h2>Filmes de Romance</h2>");
-    resultado.innerHTML += ('<div class="conteudo"><div class="imagem"><a href=' + linkRomance[i] + ' target="_blank"><img src=' + imgRomance[i] + ' class="cartaz"></a></div> <h3>' + nomeRomance[i] + '</h3></div>');
+    resultado.innerHTML += ('<section class="conteudo"><section class="imagem"><a href=' + linkRomance[i] + ' target="_blank"><img src=' + imgRomance[i] + ' class="cartaz"></a></section> <h3>' + nomeRomance[i] + '</h3></section>');
   }
   }
 
 function acao(){
-  
+
   var imgAcao = ["https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"];
   var linkAcao = ["https://www.imdb.com/title/tt0232500/?ref_=fn_al_tt_1"];
   var nomeAcao = ["Velozes e Furiosos"];
@@ -44,7 +42,7 @@ function acao(){
   for (var i = 0; i < imgAcao.length; i++) {
     var categoria = document.getElementById("categoria");
     categoria.innerHTML = ("<h2>Filmes de Ação</h2>");
-    resultado.innerHTML += ('<div class="conteudo"><div class="imagem"><a href=' + linkAcao[i] + ' target="_blank"><img src=' + imgAcao[i] + ' class="cartaz"></a></div> <h3>' + nomeAcao[i] + '</h3></div>');
+    resultado.innerHTML += ('<section class="conteudo"><section class="imagem"><a href=' + linkAcao[i] + ' target="_blank"><img src=' + imgAcao[i] + ' class="cartaz"></a></section> <h3>' + nomeAcao[i] + '</h3></section>');
   }
   }
 
@@ -60,7 +58,7 @@ function ficcaoCientifica(){
   for (var i = 0; i < imgFiccaoCientifica.length; i++) {
     var categoria = document.getElementById("categoria");
     categoria.innerHTML = ("<h2>Filmes de Ficção Científica</h2>");
-    resultado.innerHTML += ('<div class="conteudo"><div class="imagem"><a href=' + linkFiccaoCientifica[i] + ' target="_blank"><img src=' + imgFiccaoCientifica[i] + ' class="cartaz"></a></div> <h3>' + nomeFiccaoCientifica[i] + '</h3></div>');
+    resultado.innerHTML += ('<section class="conteudo"><section class="imagem"><a href=' + linkFiccaoCientifica[i] + ' target="_blank"><img src=' + imgFiccaoCientifica[i] + ' class="cartaz"></a></section> <h3>' + nomeFiccaoCientifica[i] + '</h3></section>');
   }
   }
 
@@ -76,7 +74,7 @@ function documentario(){
   for (var i = 0; i < imgDocumentario.length; i++) {
     var categoria = document.getElementById("categoria");
     categoria.innerHTML = ("<h2>Documentários</h2>");
-    resultado.innerHTML += ('<div class="conteudo"><div class="imagem"><a href=' + linkDocumentario[i] + ' target="_blank"><img src=' + imgDocumentario[i] + ' class="cartaz"></a></div> <h3>' + nomeDocumentario[i] + '</h3></div>');
+    resultado.innerHTML += ('<section class="conteudo"><section class="imagem"><a href=' + linkDocumentario[i] + ' target="_blank"><img src=' + imgDocumentario[i] + ' class="cartaz"></a></section> <h3>' + nomeDocumentario[i] + '</h3></section>');
   }
   }
 
@@ -92,7 +90,7 @@ function serie(){
   for (var i = 0; i < imgSerie.length; i++) {
     var categoria = document.getElementById("categoria");
     categoria.innerHTML = ("<h2>Séries</h2>");
-    resultado.innerHTML += ('<div class="conteudo"><div class="imagem"><a href=' + linkSerie[i] + ' target="_blank"><img src=' + imgSerie[i] + ' class="cartaz"></a></div> <h3>' + nomeSerie[i] + '</h3></div>');
+    resultado.innerHTML += ('<section class="conteudo"><section class="imagem"><a href=' + linkSerie[i] + ' target="_blank"><img src=' + imgSerie[i] + ' class="cartaz"></a></section> <h3>' + nomeSerie[i] + '</h3></section>');
   }
   }
 
@@ -108,6 +106,6 @@ function semCategoria(){
   for (var i = 0; i < imgSemCategoria.length; i++) {
     var categoria = document.getElementById("categoria");
     categoria.innerHTML = ("<h2>Sem Categoria</h2>");
-    resultado.innerHTML += ('<div class="conteudo"><div class="imagem"><a href=' + linkSemCategoria[i] + ' target="_blank"><img src=' + imgSemCategoria[i] + ' class="cartaz"></a></div> <h3>' + nomeSemCategoria[i] + '</h3></div>');
+    resultado.innerHTML += ('<section class="conteudo"><section class="imagem"><a href=' + linkSemCategoria[i] + ' target="_blank"><img src=' + imgSemCategoria[i] + ' class="cartaz"></a></section> <h3>' + nomeSemCategoria[i] + '</h3></section>');
   }
   }
